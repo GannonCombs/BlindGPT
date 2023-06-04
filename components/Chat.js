4ec09bd81d8995a5cc16d5654fac537754a44577
unknown <gacombs@yahoo.com>
2023-06-03 22:25:25 -0500

    Duplication issue, bolding issue, and list issue finally all gone together.

--- components/Chat.js
+++ components/Chat.js
@@ -4,14 +4,16 @@ import chatStyles from "../styles/Chat.module.css"
 
 const Chat = ({ conversation }) => (
   <div className={chatStyles.chat}>
-    {conversation.map((message, i) => (
-      <p key={i} className={i % 2 === 0 ? 'you' : 'ai'}>
-        {/* <strong>{i % 2 === 0 ? 'You' : 'AI'}:</strong> */}
-         {message}
-      </p>
-    ))}
+    {conversation.map((message, i) => {
+      const [prefix, ...textParts] = message.split(':');
+      const text = textParts.join(':').trim();
+      return (
+        <p key={i} className={i % 2 === 0 ? 'you' : 'ai'}>
+          <strong>{prefix}:</strong> {text}
+        </p>
+      );
+    })}
   </div>
 );
-
 
 export default Chat;
